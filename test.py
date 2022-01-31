d32ebc11f178a5e41e78f8fca8ead3bd5466d6f7
Tonjacco <tonjacco@gmail.com>
2022-01-31 16:37:03 +0100

    circle surface

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 print("hello world")
-def circirlce(radius)
+def circum_surfa_cirlce(radius)
 ""
 # input radius of a circle (unit does not matter, just use your SI brain) 
 # output the circumference
 ""
 
-cir = 2*np.pi*r
+cir = 2*np.pi*r #2pir^2
+sur = np.pi*r**2 #pir^2
+
+
