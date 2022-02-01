a06145efe3d937e447c004a976191ebdac1408c2
Tonjacco <tonjacco@gmail.com>
2022-02-01 15:21:53 +0100

    bulletpoint2

--- test.py
+++ test.py
@@ -1,15 +1,39 @@
 import numpy as np
 def C_circle(radius):
+''' input radius of a circle (unit does not matter, just use your SI brain) 
+    output the circumference of the circle
     
-# input radius of a circle (unit does not matter, just use your SI brain) 
-# output the circumference and surface of the circle
-
-    #cir = 2*np.pi*radius #2pir^2
-    sur = np.pi*radius**2 #pir^2
+    Parameters:
+    -----------
+    radius : float
+            circle radius
+    
+    Returns
+    -------
+    cir : float
+        the cirle's circumference
+'''
+    cir = 2*np.pi*radius #2pir^2
 
     return cir
 
-
+def S_circle(radius)
+''' input radius of a circle (unit does not matter, just use your SI brain) 
+    output the circumference of the circle
+    
+    Parameters:
+    -----------
+    radius : float
+            circle radius
+    
+    Returns
+    -------
+    sur : float
+        the cirle's circumference
+'''
+    sur = np.pi*radius**2 #pir^2
+    
+    return sur
 
 
 
